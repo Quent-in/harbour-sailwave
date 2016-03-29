0eec9ecbeb255daba4bb7a87c07047ba6a9b92cb
Quent-in <pages_quentin@hotmail.com>
2016-03-29 20:46:10 +0200

    French language file

---
+++ translations/harbour-sailwave-fr.ts
@@ -0,0 +1,362 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0">
+<context>
+    <name>AboutModel</name>
+    <message>
+        <location filename="../qml/common/AboutModel.qml" line="9"/>
+        <source>Project Home</source>
+        <translation type="unfinished">Site du projet</translation>
+    </message>
+    <message>
+        <location filename="../qml/common/AboutModel.qml" line="10"/>
+        <source>on GitHub</source>
+        <translation type="unfinished">sur GitHub</translation>
+    </message>
+    <message>
+        <location filename="../qml/common/AboutModel.qml" line="11"/>
+        <location filename="../qml/common/AboutModel.qml" line="18"/>
+        <location filename="../qml/common/AboutModel.qml" line="24"/>
+        <source>Project</source>
+        <translation type="unfinished">Projet</translation>
+    </message>
+    <message>
+        <location filename="../qml/common/AboutModel.qml" line="12"/>
+        <location filename="../qml/common/AboutModel.qml" line="25"/>
+        <location filename="../qml/common/AboutModel.qml" line="39"/>
+        <location filename="../qml/common/AboutModel.qml" line="46"/>
+        <location filename="../qml/common/AboutModel.qml" line="61"/>
+        <location filename="../qml/common/AboutModel.qml" line="68"/>
+        <source>Link will open</source>
+        <translation type="unfinished">Le lien s'ouvrira</translation>
+    </message>
+    <message>
+        <location filename="../qml/common/AboutModel.qml" line="16"/>
+        <source>License</source>
+        <translation type="unfinished">Licence</translation>
+    </message>
+    <message>
+        <location filename="../qml/common/AboutModel.qml" line="22"/>
+        <source>Report a bug or send a feature request</source>
+        <translation type="unfinished">Remonter un bug ou envoyer une demande de fonctionnalité</translation>
+    </message>
+    <message>
+        <location filename="../qml/common/AboutModel.qml" line="23"/>
+        <source>on GitHub Issues</source>
+        <translation type="unfinished">sur GitHub Issues</translation>
+    </message>
+    <message>
+        <location filename="../qml/common/AboutModel.qml" line="29"/>
+        <location filename="../qml/common/AboutModel.qml" line="31"/>
+        <location filename="../qml/common/AboutModel.qml" line="38"/>
+        <location filename="../qml/common/AboutModel.qml" line="45"/>
+        <source>Author</source>
+        <translation type="unfinished">Auteur</translation>
+    </message>
+    <message>
+        <location filename="../qml/common/AboutModel.qml" line="32"/>
+        <source>Mail app will open</source>
+        <translation type="unfinished">L'application de courriel s'ouvrira</translation>
+    </message>
+    <message>
+        <location filename="../qml/common/AboutModel.qml" line="36"/>
+        <source>Twitter</source>
+        <translation type="unfinished">Twitter</translation>
+    </message>
+    <message>
+        <location filename="../qml/common/AboutModel.qml" line="43"/>
+        <source>Donate</source>
+        <translation type="unfinished">Faire un don</translation>
+    </message>
+    <message>
+        <location filename="../qml/common/AboutModel.qml" line="44"/>
+        <source>via PayPal</source>
+        <translation type="unfinished">via PayPal</translation>
+    </message>
+    <message>
+        <location filename="../qml/common/AboutModel.qml" line="51"/>
+        <source>English translation provided by</source>
+        <extracomment>Use your current language instead of &apos;English&apos;</extracomment>
+        <translation type="unfinished">Traduction française fournie par</translation>
+    </message>
+    <message>
+        <location filename="../qml/common/AboutModel.qml" line="54"/>
+        <source>Michael Neufing</source>
+        <extracomment>Insert the Name(s) of the translator(s) here</extracomment>
+        <translation type="unfinished">Quentin PAGÈS</translation>
+    </message>
+    <message>
+        <location filename="../qml/common/AboutModel.qml" line="55"/>
+        <location filename="../qml/common/AboutModel.qml" line="60"/>
+        <source>Translation</source>
+        <translation type="unfinished">Traduction</translation>
+    </message>
+    <message>
+        <location filename="../qml/common/AboutModel.qml" line="58"/>
+        <source>Translate this app</source>
+        <translation type="unfinished">Traduisez cette application</translation>
+    </message>
+    <message>
+        <location filename="../qml/common/AboutModel.qml" line="59"/>
+        <source>on Transifex</source>
+        <translation type="unfinished">sur Transifex</translation>
+    </message>
+    <message>
+        <location filename="../qml/common/AboutModel.qml" line="65"/>
+        <source>Radio icon created by</source>
+        <translation type="unfinished">Icône radio créée par</translation>
+    </message>
+    <message>
+        <location filename="../qml/common/AboutModel.qml" line="67"/>
+        <source>Other</source>
+        <translation type="unfinished">Autre</translation>
+    </message>
+</context>
+<context>
+    <name>AboutPage</name>
+    <message>
+        <location filename="../qml/pages/AboutPage.qml" line="17"/>
+        <source>About SailWave</source>
+        <translation type="unfinished">À propos de SailWave</translation>
+    </message>
+</context>
+<context>
+    <name>AudioPlayer</name>
+    <message>
+        <location filename="../qml/common/AudioPlayer.qml" line="307"/>
+        <source>The stream URL is not valid.</source>
+        <translation type="unfinished">Le flux de l'URL n'est pas valide</translation>
+    </message>
+    <message>
+        <location filename="../qml/common/AudioPlayer.qml" line="311"/>
+        <source>The stream format is not supported.</source>
+        <translation type="unfinished">Le format du flux n'est pas supporté</translation>
+    </message>
+    <message>
+        <location filename="../qml/common/AudioPlayer.qml" line="315"/>
+        <source>The network is not available.</source>
+        <translation type="unfinished">Le réseau est indisponible</translation>
+    </message>
+    <message>
+        <location filename="../qml/common/AudioPlayer.qml" line="319"/>
+        <source>No permessions to access the stream.</source>
+        <translation type="unfinished">Aucune premissions pour accéder au flux.</translation>
+    </message>
+    <message>
+        <location filename="../qml/common/AudioPlayer.qml" line="323"/>
+        <source>The audio service could not be instantiated.</source>
+        <translation type="unfinished">Le service audio ne peut pas être instancié</translation>
+    </message>
+    <message>
+        <location filename="../qml/common/AudioPlayer.qml" line="327"/>
+        <source>An unknown error occured.</source>
+        <translation type="unfinished">Une erreur inconnue s'est produite</translation>
+    </message>
+    <message>
+        <location filename="../qml/common/AudioPlayer.qml" line="247"/>
+        <source>Buffering...</source>
+        <translation type="unfinished">Mise en cache...</translation>
+    </message>
+    <message>
+        <location filename="../qml/common/AudioPlayer.qml" line="250"/>
+        <source>Loading...</source>
+        <translation type="unfinished">Chargement...</translation>
+    </message>
+</context>
+<context>
+    <name>CheckStationPage</name>
+    <message>
+        <location filename="../qml/pages/CheckStationPage.qml" line="22"/>
+        <source>Validating station</source>
+        <translation type="unfinished">Validation de la station</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/CheckStationPage.qml" line="23"/>
+        <source>Cancel</source>
+        <translation type="unfinished">Annuler</translation>
+    </message>
+</context>
+<context>
+    <name>CoverPage</name>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="82"/>
+        <source>Buffering...</source>
+        <translation type="unfinished">Mise en cache...</translation>
+    </message>
+    <message>
+        <location filename="../qml/cover/CoverPage.qml" line="86"/>
+        <source>Loading...</source>
+        <translation type="unfinished">Chargement...</translation>
+    </message>
+</context>
+<context>
+    <name>DurationFormatter</name>
+    <message>
+        <location filename="../qml/js/DurationFormatter.js" line="19"/>
+        <source>HH:mm</source>
+        <extracomment>Time format for small sized labels, Hours &gt; 0.</extracomment>
+        <translation type="unfinished">HH:mm</translation>
+    </message>
+    <message>
+        <location filename="../qml/js/DurationFormatter.js" line="9"/>
+        <location filename="../qml/js/DurationFormatter.js" line="22"/>
+        <source>mm:ss</source>
+        <extracomment>Time format for playback time, Hours = 0.
+----------
+Time format for small sized labels, Hours = 0.</extracomment>
+        <translation type="unfinished">>mm:ss</translation>
+    </message>
+    <message>
+        <location filename="../qml/js/DurationFormatter.js" line="6"/>
+        <source>HH:mm:ss</source>
+        <extracomment>Time format for playback time, Hours &gt; 0.</extracomment>
+        <translation type="unfinished">HH:mm:ss</translation>
+    </message>
+    <message>
+        <location filename="../qml/js/DurationFormatter.js" line="28"/>
+        <source>HH&apos;h&apos; mm&apos;m&apos; ss&apos;s&apos;</source>
+        <extracomment>Time format for normal (large) sized labels, Hours &gt; 0.</extracomment>
+        <translation type="unfinished">HH&apos;h&apos; mm&apos;m&apos; ss&apos;s&apos;</translation>
+    </message>
+    <message>
+        <location filename="../qml/js/DurationFormatter.js" line="32"/>
+        <source>mm&apos;m&apos; ss&apos;s&apos;</source>
+        <extracomment>Time format for normal (large) sized labels, Hours = 0.</extracomment>
+        <translation type="unfinished">mm&apos;m&apos; ss&apos;s&apos;</translation>
+    </message>
+</context>
+<context>
+    <name>LicensePage</name>
+    <message>
+        <location filename="../qml/pages/LicensePage.qml" line="21"/>
+        <source>License</source>
+        <translation type="unfinished">Licence</translation>
+    </message>
+</context>
+<context>
+    <name>MainPage</name>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="47"/>
+        <source>About</source>
+        <translation type="unfinished">À propos</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="54"/>
+        <source>Settings</source>
+        <translation type="unfinished">Paramètres</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="61"/>
+        <source>Sleep timer</source>
+        <translation type="unfinished">Minuteur d'extinction</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="67"/>
+        <source>Sleep timer gets stopped</source>
+        <translation type="unfinished">Le minuteur d'extinction a été arrêté</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="77"/>
+        <source>Add station</source>
+        <translation type="unfinished">Ajouter une station</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="93"/>
+        <source>Stations</source>
+        <translation type="unfinished">Stations radio</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/MainPage.qml" line="97"/>
+        <source>No stations available.&lt;br/&gt;Pull down to add a new station.</source>
+        <translation type="unfinished">Aucune station disponible.&lt;br/&gt; Faites descendre le menu pour ajouter une nouvelle station de radio.</translation>
+    </message>
+</context>
+<context>
+    <name>SettingsDialog</name>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="25"/>
+        <source>Settings</source>
+        <translation type="unfinished">Paramètres</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="26"/>
+        <source>Save</source>
+        <translation type="unfinished">Enregistrer</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="27"/>
+        <source>Cancel</source>
+        <translation type="unfinished">Annuler</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="33"/>
+        <source>Validate the stream URL</source>
+        <translation type="unfinished">Valider le flux de l'URL</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="34"/>
+        <source>On adding or changing a station, check if the stream is valid and has a supported format.</source>
+        <translation type="unfinished">À l'ajout ou la modification d'une station, vérifier la validité du flux et de son format.</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="40"/>
+        <source>Show detailed error messages</source>
+        <translation type="unfinished">Afficher les messages d'erreu</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/SettingsDialog.qml" line="41"/>
+        <source>Show detailed error messages in notifications, reveived from phones multimedia engine.</source>
+        <translation type="unfinished">Afficher les messages d'erreur dans les notification, reçu depuis le moteur multimédia du téléphone.</translation>
+    </message>
+</context>
+<context>
+    <name>SleepTimerDialog</name>
+    <message>
+        <location filename="../qml/pages/SleepTimerDialog.qml" line="10"/>
+        <source>Sleep timer</source>
+        <translation type="unfinished">Minuterie d'extinction</translation>
+    </message>
+</context>
+<context>
+    <name>StationDelegate</name>
+    <message>
+        <location filename="../qml/common/StationDelegate.qml" line="17"/>
+        <source>Deleting</source>
+        <extracomment>Station gets deleted through context menu (remorse)</extracomment>
+        <translation type="unfinished">Suppression</translation>
+    </message>
+    <message>
+        <location filename="../qml/common/StationDelegate.qml" line="54"/>
+        <source>Edit</source>
+        <translation type="unfinished">Modifier</translation>
+    </message>
+    <message>
+        <location filename="../qml/common/StationDelegate.qml" line="65"/>
+        <source>Remove</source>
+        <translation type="unfinished">Retirer</translation>
+    </message>
+</context>
+<context>
+    <name>StationEditDialog</name>
+    <message>
+        <location filename="../qml/pages/StationEditDialog.qml" line="44"/>
+        <source>Add</source>
+        <translation type="unfinished">Ajouter</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StationEditDialog.qml" line="44"/>
+        <source>Save</source>
+        <translation type="unfinished">Enregistrer</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StationEditDialog.qml" line="56"/>
+        <source>Name</source>
+        <translation type="unfinished">Nom</translation>
+    </message>
+    <message>
+        <location filename="../qml/pages/StationEditDialog.qml" line="69"/>
+        <source>Stream URL</source>
+        <translation type="unfinished">URL du flux</translation>
+    </message>
+</context>
+</TS>
